19cca0a109f66d0b6b2a10b44e50d713adb90d46
cuongvt19 <cuong.tran.vt19@gmail.com>
2022-05-19 16:39:26 +0700

    1st Commit

---
+++ PRN_SE1622_DemoOOP/Entities/Address.cs
@@ -0,0 +1,27 @@
+﻿namespace Prn.Se1622;
+
+public class Address
+{
+    public int Number { get; set; }
+    public string Ward { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+
+    public Address()
+    {
+
+    }
+
+    public Address(int number, string ward, string street, string city)
+    {
+        Number = number;
+        Ward = ward;
+        Street = street;
+        City = city;
+    }
+
+    public override string? ToString()
+    {
+        return $"[.No = {Number}, Ward = {Ward}, Street = {Street}, City = {City}";
+    }
+}
---
+++ PRN_SE1622_DemoOOP/Entities/Student.cs
@@ -0,0 +1,50 @@
+﻿namespace Prn.Se1622;
+public class Student
+{
+    /*Fields/Data*/
+
+    private int _id;
+    private string _name;
+    private DateTime _dob;
+    private string _address;
+
+    /*Properties*/
+    //Init-auto Properties
+    public int Id { get; init; }
+    public string Name { get => _name; set => _name = value; }
+    //Read-only Auto Prpperties
+    public DateTime Dob { get; }
+    public string Address
+    {
+        get { return _address; }
+        set { _address = value; }
+    }
+    public double Mark { get; set; }
+    
+    //Construtor => ham khoi tao
+    public Student()
+    {
+
+    }
+
+    public Student(int id, string name, DateTime dob, string address, double mark)
+    {
+        Id = id;
+        Name = name;
+        Dob = dob;
+        Address = address;
+        Mark = mark;
+    }
+
+    //To increase Mark by a percentage rate
+    public void IncreaseMarkByPercent(int? percent)
+    {
+        if (percent is not null && percent > 0 && percent <= 100)
+        {
+            this.Mark = this.Mark * percent.Value / 100;
+        }
+    }
+
+    //Expression Bodied
+    public override string? ToString() => $"Id = {Id}, Name = {Name}, Date of Birth = {Dob}, Address = {Address}, Mark = {Mark}";   
+}
