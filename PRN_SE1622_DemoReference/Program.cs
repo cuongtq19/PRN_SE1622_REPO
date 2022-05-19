d5060dd28a37344ef818aa155ec4140c6525967e
cuongvt19 <cuong.tran.vt19@gmail.com>
2022-05-19 15:01:36 +0700

    Add project files.

---
+++ PRN_SE1622_DemoReference/Program.cs
@@ -0,0 +1,11 @@
+﻿static void Main()
+{
+    int x = 1, y = 2;
+    void ShowValue(int a)
+    {
+        Console.WriteLine($"x = {x}, a = {a}");
+        x = 10;
+    }
+    ShowValue(11);
+    Console.WriteLine($"x = {x}");
+}
---
+++ PRN_SE1622_PRO/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace fptedu.se1622; //.NET 6
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($"Hello, World....");
+
+        //declare more variables
+        Console.WriteLine($"Input x and y: ");
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+        var salary = 100.5;//System.Double (CTS) double
+        dynamic age = 20;//int32
+        age = true;//boolean
+
+        Console.WriteLine(salary.GetType().Name);
+        Console.WriteLine($"Total of a = {x} and b = {y} is: {Sum(x,y)}");
+        //Console.WriteLine(args[0]); /run cmd with param into args
+
+        Console.ReadLine();
+    }
+
+    public static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+}
